3495232502d47ba6ccf3f2a38040c49be2c7be9e
Camilo Italo <alpha19ci@gmail.com>
2022-12-21 00:41:06 -0300

    script: created variables

---
+++ script.js
@@ -0,0 +1,10 @@
+var minutesInput = document.getElementById("minutesInput")
+var secondsInput = document.getElementById("secondsInput")
+var startButton = document.getElementById("startButton")
+var progressBar = document.getElementById("progressBar")
+var colonStyle = document.getElementById("colonStyle")
+
+var progressBarMax
+var changeSeconds = 0
+var changeMinutes = 0
+var stopper
